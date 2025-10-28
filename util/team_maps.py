b7cbb7f61aa216f9f5cf78733b94a92346abda1c
Sohraub Pazuki <sohraub.pazuki@arctiq.ca>
2025-10-28 11:11:59 -0400

    add old utah team back to mappings

--- util/team_maps.py
+++ util/team_maps.py
@@ -31,5 +31,6 @@ nst_team_mapping = {
     "Chicago Blackhawks": "CHI",
     "Arizona Coyotes": "ARI",
     "Utah Mammoth": "UTA",
+    "Utah Hockey Team": "UTA",
     "New York Islanders": "NYI"
 }
